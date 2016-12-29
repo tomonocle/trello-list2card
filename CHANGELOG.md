11b42dabdce8b061f90f42ee17cd98e1607f45a0
tomonocle <github@woot.co.uk>
2016-12-29 20:56:36 +0000

    Bump versions

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+# 0.1.1
+- Fix incorrect installation instructions
+
 # 0.1.0
-Initial release
+- Initial release
--- lib/trello/list2card/version.rb
+++ lib/trello/list2card/version.rb
@@ -1,5 +1,5 @@
 module Trello
     class List2Card
-        VERSION = '0.1.0'
+        VERSION = '0.1.1'
     end
 end
